d934c439fb207dc181ddbb82d5e3728637e2a02f
Joel Crawford <macbook@Crawford.local>
2023-04-27 09:19:39 +0300

    Delete items Touchopacity

--- App.js
+++ App.js
@@ -1,9 +1,24 @@
 import { React, useState } from 'react';
-import { KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { Keyboard, KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import Task from './components/Task';
 
 export default function App() {
   const [task, setTask] = useState(null);
+  const [taskItems, setTaskItems] = useState([]);
+
+  const handleAddTask = () => {
+    //Dismiss keyboard on add task 
+    Keyboard.dismiss();
+    setTaskItems([...taskItems, task]);
+    setTask(null);
+  }
+
+  const handleDeletetask = (index) => {
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy);
+
+  }
 
   return (
     <View style={styles.container}>
@@ -11,16 +26,20 @@ export default function App() {
         <Text style={styles.sectionTitle}>Today's Task</Text>
 
         <View style={styles.items}>
+          {taskItems.map((item, index) => {
+            return (<TouchableOpacity key={index} onPress={() => handleDeletetask(index)}>
+              <Task text={item} />
+            </TouchableOpacity>)
+          })}
           {/* This is where the task will go */}
-          <Task text="First component" />
-          <Task text="Second component" />
+
         </View>
       </View>
 
       {/* Write a task */}
       <KeyboardAvoidingView behavior={Platform.OS === 'iOS' ? 'padding' : 'height'} style={styles.writeTaskWrapper}>
-        <TextInput style={styles.input} placeholder={"Write a Task..."} />
-        <TouchableOpacity >
+        <TextInput style={styles.input} placeholder={"Write a Task..."} value={task} onChangeText={text => setTask(text)} />
+        <TouchableOpacity onPress={() => handleAddTask()}>
           <View style={styles.addWrapper}>
             <Text style={styles.addText}>+</Text>
           </View>
